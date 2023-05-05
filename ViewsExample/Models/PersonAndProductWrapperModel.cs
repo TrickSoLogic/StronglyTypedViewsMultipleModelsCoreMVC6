0bbf6626280e4a50287ddace7b3c1c4c2aa9c707
Vishal <vishalbhat.com@gmail.com>
2023-05-05 17:01:22 +0530

    Add project files.

---
+++ ViewsExample/Models/PersonAndProductWrapperModel.cs
@@ -0,0 +1,8 @@
+﻿namespace StronglyTypedViewsMultipleModelsCoreMVC6.Models
+{
+  public class PersonAndProductWrapperModel
+  {
+    public Person PersonData { get; set; }
+    public Product ProductData { get; set; }
+  }
+}
